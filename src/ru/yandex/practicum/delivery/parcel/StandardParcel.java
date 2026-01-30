86cc1bf40778f3439168a6fb8ebac8b2df5bf621
topzip <topzip2142@yandex.ru>
2026-01-30 12:32:56 +0300

    Дополнен дочерний класс стандартных посылок

---
+++ src/ru/yandex/practicum/delivery/parcel/StandardParcel.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.delivery.parcel;
+
+public class StandardParcel extends Parcel{
+
+
+    public StandardParcel(String description, int weight, String deliveryAddress, int sendDay) {
+        super(description, weight, deliveryAddress, sendDay);
+    }
+
+    @Override
+    public int getBaseCost() {
+        return Parcel.BASE_COST_STANDARD;
+    }
+
+}
