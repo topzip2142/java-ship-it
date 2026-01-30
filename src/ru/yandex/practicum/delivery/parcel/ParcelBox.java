924c442d88059aab9412d37f3d1d77b045b44c38
topzip <topzip2142@yandex.ru>
2026-01-30 12:31:49 +0300

    Реализован класс коробок с полылками

---
+++ src/ru/yandex/practicum/delivery/parcel/ParcelBox.java
@@ -0,0 +1,38 @@
+package ru.yandex.practicum.delivery.parcel;
+
+import java.util.ArrayList;
+
+public class ParcelBox<T extends Parcel> {
+    private final int maxWeight;
+    private int weight;
+    private final ArrayList<T> parcels = new ArrayList<>();
+
+    public ParcelBox(int maxWeight) {
+        this.maxWeight = maxWeight;
+        weight = 0;
+    }
+
+    public void addParcel(T parcel) {
+        if (weight >= maxWeight) {
+            System.out.println("Коробка заполнена, "
+                    + "невозможно переместить в неё посылку <<" + parcel.getDescription() + ">>");
+            return;
+        }
+
+        parcels.add(parcel);
+        weight += parcel.getWeight();
+
+    }
+
+    public void getAllParcels() {
+        if (parcels.isEmpty()) {
+            System.out.println("Данная коробка пуста");
+            return;
+        }
+
+        System.out.println("В данной коробке лежат посылки:");
+        for (T parcel : parcels) {
+            System.out.println("\t" + parcel.getDescription());
+        }
+    }
+}
